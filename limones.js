fb345d0d92e9c6328042353c69daf44d7a9086ff
Addo1996 <addo-fran@hotmail.es>
2026-04-14 12:00:28 -0500

    agregando canvas

--- limones.js
+++ limones.js
@@ -1,2 +1,8 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+function dibujarSuelo(){
+    ctx.fillStyle="blue";
+    ctx.fillRect(0, canvas.height-20, canvas.width, 20);
+
+}
